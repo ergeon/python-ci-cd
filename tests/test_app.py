8f65ab7f0350bc26f696160f8c29a5fda34eab40
Alexey Evseev @stalk <myhappydo@gmail.com>
2022-03-15 19:33:08 +0300

    Improve coverage

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,9 @@
-from src.app import main, echo
+from src.app import main, echo, do_fireworks
 
 
 def test_main():
     assert main() == 0
+
+
+def test_do_fireworks():
+    assert do_fireworks() == 'tada!'
