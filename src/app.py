eb4a21a776ba387e469ab5fe64292c1bb5a5bf72
Chapayev Dauren <chapaevdauren@gmail.com>
2022-05-25 13:57:25 +0600

    [ENG-14831] Push merged coverage report to coveralls

--- src/app.py
+++ src/app.py
@@ -19,3 +19,19 @@ def say_hello():
 
 def new_24():
     return 'value 24'
+
+
+def add(a, b):
+    return a + b
+
+
+def minus(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def power(a, b):
+    return a ** b
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
-from src.app import main, echo
+from src.app import main, echo, add, minus, multiply, power
 
 
 def test_main():
@@ -7,3 +7,19 @@ def test_main():
 
 def test_echo():
     assert echo('whale') == 'whale'
+
+
+def test_add():
+    assert add(1, 1) == 2
+
+
+def test_minus():
+    assert minus(1, 1) == 0
+
+
+def test_multiply():
+    assert multiply(2, 1) == 2
+
+
+def test_power():
+    assert power(2, 1) == 2
